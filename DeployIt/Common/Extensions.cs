a187992d9338eb0cc87cc646f507d12545165306
zeng.stephen@gmail.com <zeng.stephen@gmail.com>
2014-11-12 22:19:11 +0800

    Adjust layout; Added extensions class.

---
+++ DeployIt/Common/Extensions.cs
@@ -0,0 +1,20 @@
+namespace DeployIt.Common
+{
+    public static class Extensions
+    {
+        public static T? ToNullable<T>(this object input) where T : struct
+        {
+            if (input == null) return default(T?);
+
+            if (input is T?) return (T?) input;
+            
+            int temp;
+            if (int.TryParse(input.ToString(), out temp))
+            {
+                return temp as T?;
+            }
+            
+            return default(T?);
+        }
+    }
+}
--- DeployIt/Common/Helper.cs
+++ DeployIt/Common/Helper.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Xml;
 using DeployIt.Models;
-using Microsoft.TeamFoundation.Build.Client;
-using Microsoft.TeamFoundation.Client;
+using Raven.Client;
 
 namespace DeployIt.Common
 {
