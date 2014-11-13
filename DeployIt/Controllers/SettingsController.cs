27ba6ae6e5d35c2caf6259a3380c35ad5e61456c
Stephen Zeng <zeng.stephen@gmail.com>
2014-11-13 10:08:43 +0800

    Fixed the reference issue

--- DeployIt/Controllers/SettingsController.cs
+++ DeployIt/Controllers/SettingsController.cs
@@ -11,5 +11,23 @@ namespace DeployIt.Controllers
 
             return View(list);
         }
+
+        public ActionResult Test()
+        {
+            var config = new ProjectConfig
+            {
+                Name = "DMS",
+                TfsProjectName = "DSC.DMS",
+                Branch = "DMS-Main",
+                VersionKeyName = "Version",
+                SourceSubFolder = "",
+                DestinationRootLocation = "",
+                DetinationProjectFolder = "DMS_Main",
+            };
+
+            DocumentSession.Store(config);
+
+            return View();
+        }
     }
 }
