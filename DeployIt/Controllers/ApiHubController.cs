bfc97244e03840c605bab9ab68f06082669ef0d0
zeng.stephen@gmail.com <zeng.stephen@gmail.com>
2014-11-13 21:39:32 +0800

    Implementing SignalR hub

---
+++ DeployIt/Controllers/ApiHubController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Web.Http;
+using Microsoft.AspNet.SignalR;
+
+namespace DeployIt.Controllers
+{
+    public abstract class ApiHubController<T> : ApiController where T : Hub
+    {
+        private readonly Lazy<IHubContext> _lazy = new Lazy<IHubContext>(() => GlobalHost.ConnectionManager.GetHubContext<T>());
+
+        public IHubContext HubContext
+        {
+            get { return _lazy.Value; }
+        }
+    }
+}
---
+++ DeployIt/Hubs/NotificationHub.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace DeployIt.Hubs
+{
+    public class NotificationHub : Hub
+    {
+    }
+}
