280a4abb9c04779f29cff03dd1d28d0e46eef276
zeng.stephen@gmail.com <zeng.stephen@gmail.com>
2014-11-13 21:49:13 +0800

    added deploy api controller

---
+++ DeployIt/Controllers/DeployController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using DeployIt.Hubs;
+using DeployIt.Models;
+
+namespace DeployIt.Controllers
+{
+    public class DeployController : ApiHubController<NotificationHub>
+    {
+        public HttpResponseMessage Get()
+        {
+            HubContext.Clients.All.broadcast(string.Format("Get: The server time is {0}", DateTime.Now));
+
+            return new HttpResponseMessage(HttpStatusCode.OK);
+        }
+        public HttpResponseMessage Post(DeployBuildModel model)
+        {
+            HubContext.Clients.All.broadcast(string.Format("Post: The server time is {0}", DateTime.Now));
+
+            return new HttpResponseMessage(HttpStatusCode.OK);
+        }
+    }
+}
